7dc92db56141e402d5ff6fbbba7eeeaedb7779cc
wxsatellite <1453085314@qq.com>
2022-04-24 17:08:27 +0800

    集成gin

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"name": "wx"})
+	})
+	r.Run()
 }
