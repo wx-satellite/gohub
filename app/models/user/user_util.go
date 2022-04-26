dfe5a574173d8a52905bd5ca71e4bd5c8e3f3e28
wxsatellite <1453085314@qq.com>
2022-04-26 16:07:24 +0800

    fix

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -3,6 +3,7 @@ package user
 import "gohub/pkg/database"
 
 // IsEmailExist 判断邮箱是否已经被注册
+// 因为包名是 user，所以调用的形式是 user.IsEmailExist，因此不需要在函数名中加入 user
 func IsEmailExist(email string) bool {
 	var count int64
 	database.DB.Model(&User{}).Where("email = ?", email).Count(&count)
